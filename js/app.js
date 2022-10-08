987a2507742cd78f9047d28d474496d1d6c32e8c
Lyndsay Futrell <lyndsayfutrell@gmail.com>
2022-10-08 18:24:41 -0400

    added score functionality

--- js/app.js
+++ js/app.js
@@ -6,6 +6,7 @@ const answers = document.querySelector('.answers')
 answers.addEventListener('click', (e)=>{
     game.handleInteraction(e);
     answers.className = "answers active";
+    updateScore();
 })
 
 const next_button = document.querySelector('.next')
@@ -13,5 +14,19 @@ next_button.addEventListener('click', ()=>{
     if(answers.className === 'answers active'){
     game.handleShowNewQuestion();
     answers.className = 'answers'
+    questionNumber ++;
+    updateScore();
     }
 });
+
+// Score Variables
+let questionNumber = 1;
+let correctAnswers = 0;
+const scoreBoard = document.querySelector('p');
+
+function updateScore(){
+    scoreBoard.innerHTML = `Your score: ${correctAnswers}/${questionNumber}`;
+}
+
+//Start score at question 1
+updateScore();
