f8f1bc97eb755be42ec526647332834efa39b898
Sahil <sahilsaxena9450@gmail.com>
2022-10-13 18:42:19 +0530

    Scramble the orders of the options

--- js/question.js
+++ js/question.js
@@ -1,40 +1,49 @@
-class Question{
-    constructor(question){
-        this.question = question
-    }
+class Question {
+  constructor(question) {
+    this.question = question;
+  }
 
-    addQuestionToDisplay(){
-        const question_container = document.querySelector(".quiz-container h3")
-        const answers_container = document.querySelector(".answers")
+  addQuestionToDisplay() {
+    const question_container = document.querySelector(".quiz-container h3");
+    const answers_container = document.querySelector(".answers");
 
-        question_container.innerHTML = this.question.question
-        let answers = [this.question.correct_answer].concat(this.question.incorrect_answers)
-        let answers_html = ""
-        answers.forEach((answer)=>{
-            answers_html += `<div class='answer'>${answer}</div>`
-        })
-        answers_html += ""
-        answers_container.innerHTML = answers_html
+    question_container.innerHTML = this.question.question;
+    let answers = [this.question.correct_answer].concat(
+      this.question.incorrect_answers
+    );
 
-    }
+      let temp = [];
+      answers.forEach((e) => {
+        let r = Math.floor(answers.length * Math.random());
+        while (temp[r] !== undefined) {
+          r = Math.floor(answers.length * Math.random());
+        }
+        temp[r] = e;
+      });
+      console.log(temp);
+      answers = [...temp]
 
-    checkAnswer(answer){
-        return answer === this.question.correct_answer
-    }
+    let answers_html = "";
+    answers.forEach((answer) => {
+      answers_html += `<div class='answer'>${answer}</div>`;
+    });
+    answers_html += "";
+    answers_container.innerHTML = answers_html;
+  }
 
-    showAnswers(){
-        const answers = document.querySelectorAll(".answer")
+  checkAnswer(answer) {
+    return answer === this.question.correct_answer;
+  }
 
-        answers.forEach((answer)=>{
+  showAnswers() {
+    const answers = document.querySelectorAll(".answer");
 
-            if (this.checkAnswer(answer.textContent)){
-                answer.style.backgroundColor = "green";
-            } else {
-                answer.style.backgroundColor = "red";
-            }
-   
-        })
-
-    }
-
+    answers.forEach((answer) => {
+      if (this.checkAnswer(answer.textContent)) {
+        answer.style.backgroundColor = "green";
+      } else {
+        answer.style.backgroundColor = "red";
+      }
+    });
+  }
 }
